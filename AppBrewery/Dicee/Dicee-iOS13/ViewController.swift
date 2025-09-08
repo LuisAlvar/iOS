1d8fad530092ef81f01b34177f781e270f287fa8
Luis Alvarez <alvarez.en.luis@gmail.com>
2025-09-07 23:17:55 -0700

    done with this project

--- AppBrewery/Dicee/Dicee-iOS13/ViewController.swift
+++ AppBrewery/Dicee/Dicee-iOS13/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
+    var currentImageIndex = 0;
     
     // Our constant data source
     let arryImages = [UIImage(resource: ImageResource.diceOne),
@@ -25,12 +26,47 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        DisplayDices();
+        DisplayDices()
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         // print("Button got tapped.");
-        DisplayDices();
+        ShuffleTypeEffect()
+        DisplayDices()
+    }
+    
+    func ShuffleTypeEffect()
+    {
+        var prevIndexForDiceImage1 = 0
+        var prevIndexForDiceImage2 = 0
+        var currIndexForDiceImage1 = Int.random(in: 0...arryImages.count-1)
+        var currIndexForDiceImage2 = Int.random(in: 0...arryImages.count-1)
+        
+        let maxCounter: Int = 3
+        var iterationCounter: Int = 0
+        
+        Timer.scheduledTimer(withTimeInterval: 0.3, repeats: true) { timer in
+            if iterationCounter >= maxCounter {
+                timer.invalidate()
+                return
+            }
+            
+            while currIndexForDiceImage1 == prevIndexForDiceImage1 {
+                currIndexForDiceImage1 = Int.random(in: 0...self.arryImages.count-1)
+            }
+            
+            while currIndexForDiceImage2 == prevIndexForDiceImage2 {
+                currIndexForDiceImage2 = Int.random(in: 0...self.arryImages.count-1)
+            }
+            
+            self.diceImageView1.image = self.arryImages[currIndexForDiceImage1]
+            self.diceImageView2.image = self.arryImages[currIndexForDiceImage2]
+            
+            prevIndexForDiceImage1 = currIndexForDiceImage1
+            prevIndexForDiceImage2 = currIndexForDiceImage2
+            
+            iterationCounter += 1
+        }
     }
     
     // Randomizing the ImageViews
@@ -38,7 +74,5 @@ class ViewController: UIViewController {
         diceImageView1.image = arryImages[Int.random(in: 0...arryImages.count-1)];
         diceImageView2.image = arryImages[Int.random(in: 0...arryImages.count-1)];
     }
-
-    
 }
 
