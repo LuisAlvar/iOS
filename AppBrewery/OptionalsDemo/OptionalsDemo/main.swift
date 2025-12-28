9d6226272b4b3890882f019eb547ab95050020f8
Luis Alvarez <alvarez.en.luis@gmail.com>
2025-12-28 00:46:17 -0800

    add optionals demo pratice

--- AppBrewery/OptionalsDemo/OptionalsDemo/main.swift
+++ AppBrewery/OptionalsDemo/OptionalsDemo/main.swift
@@ -7,5 +7,50 @@
 
 import Foundation
 
-print("Hello, World!")
+var myOptionals: String?
+myOptionals = "Luis"
 
+if myOptionals != nil {
+    let safeToUse: String = myOptionals!
+    print(safeToUse)
+}
+else {
+    print("myOptional was found to be nill")
+}
+
+/*
+ A shortcut for the above. is to use the following
+ optional binding
+ */
+
+if let safeOptional = myOptionals {
+    let safeToUse: String = safeOptional
+    print(safeToUse)
+}
+
+/*
+ 
+ What if we want to set a default value if nil
+ 
+ */
+
+myOptionals = nil
+
+let text: String = myOptionals ?? "I am the default value"
+
+
+print(text)
+
+
+struct MyOptional {
+    var property = 123
+    func method() {
+        print("I am the stuct's method.")
+    }
+}
+
+let myOptional: MyOptional?
+
+myOptional = nil
+
+print(myOptional?.property)
