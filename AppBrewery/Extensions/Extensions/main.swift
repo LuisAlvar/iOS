2f4e3eb8db54d94078e0e6e09212f6559b5168d3
Luis Alvarez <alvarez.en.luis@gmail.com>
2026-01-02 17:33:18 -0800

    done with extension of custom class, native class, and protocols

--- AppBrewery/Extensions/Extensions/main.swift
+++ AppBrewery/Extensions/Extensions/main.swift
@@ -6,6 +6,32 @@
 //
 
 import Foundation
+#if canImport(UIKit)
+import UIKit
+#endif
 
-print("Hello, World!")
+extension Double {
+    func round(to places: Int) -> Double{
+        let divisor = pow(10.0, Double(places))
+        let scaled = self * divisor
+        let rounded = (scaled).rounded()
+        return rounded / divisor
+    }
+}
+
+var myDouble = 3.14159
+print(myDouble.round(to: 3))
+
+#if canImport(UIKit)
+extension UIButton {
+    func makeCircular() {
+        self.clipsToBounds = true
+        self.layer.cornerRadius = self.frame.size.width / 2
+    }
+}
+
+let button = UIButton(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+button.backgroundColor = .red
+button.makeCircular()
+#endif
 
